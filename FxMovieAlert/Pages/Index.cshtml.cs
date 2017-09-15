99ab2dbf3766ada039a6db11c06e74d1d64ad113
Wim Devos <wdevos@myforce.be>
2017-09-15 19:37:10 +0200

    FxMovieAlert: Added site.

---
+++ FxMovieAlert/Pages/Index.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FxMovies.FxMoviesDB;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace FxMovieAlert.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public IList<MovieEvent> MovieEvents = new List<MovieEvent>();
+
+        public void OnGet()
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            // Get the connection string
+            string connectionString = configuration.GetConnectionString("FxMoviesDb");
+
+            using (var db = FxMoviesDbContextFactory.Create(connectionString))
+            {
+                MovieEvents = db.MovieEvents.Include(m => m.Channel).ToList();
+            }
+        }
+    }
+}
