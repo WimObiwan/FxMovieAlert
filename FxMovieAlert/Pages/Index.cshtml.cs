34d52e08be01e1b6e7533b63f1878008d648840f
Wim Devos <wdevos@myforce.be>
2017-09-20 19:32:53 +0200

    Site: Added filtering on movies that were not yet rated.
    (cherry picked from commit 9a7dc26faa34a121d50446f44be84b333d956680)

--- FxMovieAlert/Pages/Index.cshtml.cs
+++ FxMovieAlert/Pages/Index.cshtml.cs
@@ -10,12 +10,19 @@ using Microsoft.Extensions.Configuration;
 
 namespace FxMovieAlert.Pages
 {
+    public class Record
+    {
+        public MovieEvent MovieEvent { get; set; }
+        public UserRating UserRating { get; set; }
+    }
+
     public class IndexModel : PageModel
     {
-        public IList<MovieEvent> MovieEvents = new List<MovieEvent>();
+        public IList<Record> Records = new List<Record>();
         public decimal? MinRating = null;
+        public bool NotYetRated = false;
 
-        public void OnGet(decimal? minrating = null)
+        public void OnGet(decimal? minrating = null, bool notyetrated = false)
         {
             var configuration = new ConfigurationBuilder()
                 .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
@@ -28,11 +35,23 @@ namespace FxMovieAlert.Pages
             if (minrating.HasValue)
                 MinRating = minrating.Value;
 
+            NotYetRated = notyetrated;
+
             using (var db = FxMoviesDbContextFactory.Create(connectionString))
             {
-                MovieEvents = db.MovieEvents.Include(m => m.Channel)
-                    .Where(m => !MinRating.HasValue || m.ImdbRating >= MinRating.Value * 10)
-                    .ToList();
+                Records = (
+                    from me in db.MovieEvents.Include(m => m.Channel)
+                    join ur in db.UserRatings on me.ImdbId equals ur.ImdbMovieId into urGroup
+                    from ur in urGroup.DefaultIfEmpty(null)
+                    where 
+                        (!MinRating.HasValue || me.ImdbRating >= MinRating.Value * 10)
+                        &&
+                        (!NotYetRated || ur == null)
+                    select new Record() { MovieEvent = me, UserRating = ur }
+                ).ToList();
+                // MovieEvents = db.MovieEvents.Include(m => m.Channel)
+                //     .Where(m => !MinRating.HasValue || m.ImdbRating >= MinRating.Value * 10)
+                //     .ToList();
             }
         }
     }
