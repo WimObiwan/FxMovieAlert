77b1e54f8118511132db75d830ea416cb8f69d1e
Wim Devos <wdevos@myforce.be>
2017-09-21 19:12:12 +0200

    Site: Added IMDB User settings page.
    (cherry picked from commit 2a2c7477597670134340213bc6d004500915ce7f)

---
+++ FxMovieAlert/Pages/ImdbUser.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FxMovies.FxMoviesDB;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+
+namespace FxMovieAlert.Pages
+{
+    public class ImdbUserModel : PageModel
+    {
+        public string ImdbUserId = null;
+        public DateTime? RefreshRequestTime = null;
+        public DateTime? LastRefreshRatingsTime = null;
+        public string LastRefreshRatingsResult = null;       
+        public bool? LastRefreshSuccess = null;       
+
+        public void OnGet()
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            ImdbUserId = configuration.GetSection("Temp")["ImdbUserId"];
+            string connectionString = configuration.GetConnectionString("FxMoviesDb");
+
+            using (var db = FxMoviesDbContextFactory.Create(connectionString))
+            {
+                var user = db.Users.Find(ImdbUserId);
+                if (user != null)
+                {
+                    RefreshRequestTime = user.RefreshRequestTime;
+                    LastRefreshRatingsTime = user.LastRefreshRatingsTime;
+                    LastRefreshRatingsResult = user.LastRefreshRatingsResult;
+                    LastRefreshSuccess = user.LastRefreshSuccess;
+                    user.LastUsageTime = DateTime.Now;
+                    db.SaveChanges();
+                }
+            }
+        }
+    }
+}
