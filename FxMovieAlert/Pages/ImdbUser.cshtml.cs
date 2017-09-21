9a7de2c2b696d88e429dba6db43c1b0306bab5d9
Wim Devos <wdevos@myforce.be>
2017-09-21 19:12:32 +0200

    Site: Added storing and retrieving ImdbUserId in cookie.
    (cherry picked from commit 243031ae4f39c47ee9e10caf3ff4954cf1c7ed38)

--- FxMovieAlert/Pages/ImdbUser.cshtml.cs
+++ FxMovieAlert/Pages/ImdbUser.cshtml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using FxMovies.FxMoviesDB;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -10,33 +11,61 @@ namespace FxMovieAlert.Pages
 {
     public class ImdbUserModel : PageModel
     {
+        public bool? CookieStored = false;        
         public string ImdbUserId = null;
         public DateTime? RefreshRequestTime = null;
         public DateTime? LastRefreshRatingsTime = null;
         public string LastRefreshRatingsResult = null;       
         public bool? LastRefreshSuccess = null;       
 
-        public void OnGet()
+        public void OnGet(bool forcerefresh = false, string setimdbuserid = null)
         {
             var configuration = new ConfigurationBuilder()
                 .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
                 .AddEnvironmentVariables()
                 .Build();
 
-            ImdbUserId = configuration.GetSection("Temp")["ImdbUserId"];
+            if (setimdbuserid == null)
+            {
+                ImdbUserId = Request.Cookies["ImdbUserId"];
+            }
+            else if (setimdbuserid == "remove")
+            {
+                Response.Cookies.Delete("ImdbUserId");
+                ImdbUserId = null;
+            }
+            else
+            {
+                var match = Regex.Match(setimdbuserid, @"(ur\d+)");
+                if (match.Success)
+                {
+                    var imdbuserid = match.Groups[1].Value;
+                    Response.Cookies.Append("ImdbUserId", imdbuserid);
+                    ImdbUserId = setimdbuserid;
+                    CookieStored = true;
+                }
+                else
+                {
+                    CookieStored = false;
+                }
+            }
+
             string connectionString = configuration.GetConnectionString("FxMoviesDb");
 
             using (var db = FxMoviesDbContextFactory.Create(connectionString))
             {
-                var user = db.Users.Find(ImdbUserId);
-                if (user != null)
+                if (ImdbUserId != null)
                 {
-                    RefreshRequestTime = user.RefreshRequestTime;
-                    LastRefreshRatingsTime = user.LastRefreshRatingsTime;
-                    LastRefreshRatingsResult = user.LastRefreshRatingsResult;
-                    LastRefreshSuccess = user.LastRefreshSuccess;
-                    user.LastUsageTime = DateTime.Now;
-                    db.SaveChanges();
+                    var user = db.Users.Find(ImdbUserId);
+                    if (user != null)
+                    {
+                        RefreshRequestTime = user.RefreshRequestTime;
+                        LastRefreshRatingsTime = user.LastRefreshRatingsTime;
+                        LastRefreshRatingsResult = user.LastRefreshRatingsResult;
+                        LastRefreshSuccess = user.LastRefreshSuccess;
+                        user.LastUsageTime = DateTime.Now;
+                        db.SaveChanges();
+                    }
                 }
             }
         }
