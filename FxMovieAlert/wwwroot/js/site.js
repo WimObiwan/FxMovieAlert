09be176dbe827f4f88adc160275d0b5033df57db
Wim Devos <wim@obiwan.be>
2017-10-17 23:02:45 +0200

    Site: Added vertical ad sidebar.

--- FxMovieAlert/wwwroot/js/site.js
+++ FxMovieAlert/wwwroot/js/site.js
@@ -1,1 +1,21 @@
 ﻿// Write your Javascript code.
+// Navigation Scripts to Show Header on Scroll-Up
+jQuery(document).ready(function($) {
+    var MQL = 1500;
+    $('.sideBar').fadeOut('fast');
+
+    //primary navigation slide-in effect
+    if ($(window).width() >= MQL) {
+        $(window).on('scroll', {
+            },
+            function() {
+                var currentTop = $(window).scrollTop();
+
+                if (currentTop >= 150) {
+                    $('.sideBar').fadeIn('slow');
+                } else {
+                    $('.sideBar').fadeOut('slow');
+                }
+            });
+    }
+});
