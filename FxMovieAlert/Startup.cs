44598f4d91241004f7fa0018772b93898479ec3e
Wim Devos <wim@obiwan.be>
2018-12-16 17:26:46 +0100

    Site: Removed unused "Using" directives.

--- FxMovieAlert/Startup.cs
+++ FxMovieAlert/Startup.cs
@@ -1,11 +1,9 @@
 using System;
 using System.Globalization;
-using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
