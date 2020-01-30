49f6330da1e676395519aeed614efbad960b8fab
WimObiwan <wim@obiwan.be>
2020-01-30 23:28:10 +0100

    Grabber: Disabled failing YeloGrabber (after HumoGrabber)

--- Grabber/Program.cs
+++ Grabber/Program.cs
@@ -494,7 +494,7 @@ namespace FxMovies.Grabber
                 DateTime date = now.Date.AddDays(days);
                 var movies = HumoGrabber.GetGuide(date).Result;
 
-                YeloGrabber.GetGuide(date, movies);
+                //YeloGrabber.GetGuide(date, movies);
 
                 // Remove movies that should be ignored
                 Func<MovieEvent, bool> isMovieIgnored = delegate(MovieEvent movieEvent)
