6b08c5fb3651aed0cb9176d5c09706a466d10ad8
WimObiwan <wim@obiwan.be>
2021-06-12 23:51:37 +0200

    Fixed Set Imdb ID, improved DI

--- FxMovieAlert/Pages/Index.cshtml.cs
+++ FxMovieAlert/Pages/Index.cshtml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using FxMovies.Core;
 using FxMovies.FxMoviesDB;
 using FxMovies.ImdbDB;
 using Microsoft.AspNetCore.Authentication;
@@ -82,16 +83,22 @@ namespace FxMovieAlert.Pages
         public int Count8days = 0;
         public int AdsInterval = 5;
 
-        private IConfiguration configuration;
+        private readonly IConfiguration configuration;
+        private readonly FxMoviesDbContext fxMoviesDbContext;
+        private readonly IMovieCreationHelper movieCreationHelper;
 
-        public ZendersModel(IConfiguration configuration)
+        public ZendersModel(IConfiguration configuration, FxMoviesDbContext fxMoviesDbContext,
+            IMovieCreationHelper movieCreationHelper)
         {
             this.configuration = configuration;
+            this.fxMoviesDbContext = fxMoviesDbContext;
+            this.movieCreationHelper = movieCreationHelper;
         }
 
         public void OnGet(int? m = null, int? typeMask = null, decimal? minrating = null, bool? notyetrated = null, Cert cert = Cert.all,
             int? movieeventid = null, string setimdbid = null, int? maxdays = null)
         {
+            var dbMovieEvents = fxMoviesDbContext.MovieEvents.Where(me => !me.Vod);
             string userId = ClaimChecker.UserId(User.Identity);
 
             var now = DateTime.Now;
@@ -122,159 +129,146 @@ namespace FxMovieAlert.Pages
             if (FilterCert == Cert.all2)
                 FilterCert = Cert.all;
 
-            using (var db = FxMoviesDbContextFactory.Create(connectionString))
+            if (EditImdbLinks && movieeventid.HasValue && !string.IsNullOrEmpty(setimdbid))
             {
-                if (EditImdbLinks && movieeventid.HasValue && !string.IsNullOrEmpty(setimdbid))
+                bool overwrite = false;
+                var match = Regex.Match(setimdbid, @"(tt\d+)");
+                if (match.Success)
                 {
-                    bool overwrite = false;
-                    var match = Regex.Match(setimdbid, @"(tt\d+)");
-                    if (match.Success)
+                    setimdbid = match.Groups[0].Value;
+                    overwrite = true;
+                }
+                else if (setimdbid.Equals("remove", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    setimdbid = null;
+                    overwrite = true;
+                }
+                
+                if (overwrite)
+                {
+                    var movieEvent = dbMovieEvents.SingleOrDefault(me => me.Id == movieeventid.Value);
+                    if (movieEvent != null)
                     {
-                        setimdbid = match.Groups[0].Value;
-                        overwrite = true;
-                    }
-                    else if (setimdbid.Equals("remove", StringComparison.InvariantCultureIgnoreCase))
-                    {
-                        setimdbid = null;
-                        overwrite = true;
-                    }
-                    
-                    if (overwrite)
-                    {
-                        var movieEvent = db.MovieEvents.SingleOrDefault(me => me.Id == movieeventid.Value);
-                        if (movieEvent != null)
+                        if (movieEvent.Movie != null && movieEvent.Movie.ImdbId == setimdbid)
                         {
-                            if (movieEvent.Movie.ImdbId == setimdbid)
+                            // Already ok, Do nothing
+                        }
+                        else
+                        {
+                            movieEvent.Movie = null;
+                            if (setimdbid != null)
                             {
-                                // Already ok, Do nothing
-                            }
-                            else
-                            {
-                                movieEvent.Movie = null;
-                                if (setimdbid != null)
+                                FxMovies.FxMoviesDB.Movie movie = movieCreationHelper.GetOrCreateMovieByImdbId(setimdbid);
+                                movieEvent.Movie = movie;
+                                movie.ImdbId = setimdbid; 
+
+                                using (var dbImdb = ImdbDbContextFactory.Create(connectionStringImdb))
                                 {
-                                    FxMovies.FxMoviesDB.Movie movie = null;
-
-                                    if (movie == null)
-                                        movie = db.Movies.SingleOrDefault(m => m.ImdbId == setimdbid);
-
-                                    if (movie == null)
+                                    var imdbMovie = dbImdb.Movies.SingleOrDefault(m => m.ImdbId == setimdbid);
+                                    if (imdbMovie != null)
                                     {
-                                        movie = new FxMovies.FxMoviesDB.Movie();
-                                        db.Movies.Add(movie);
-                                    }
-
-                                    movieEvent.Movie = movie;
-                                    movie.ImdbId = setimdbid; 
-
-                                    using (var dbImdb = ImdbDbContextFactory.Create(connectionStringImdb))
-                                    {
-                                        var imdbMovie = dbImdb.Movies.SingleOrDefault(m => m.ImdbId == setimdbid);
-                                        if (imdbMovie != null)
-                                        {
-                                            movieEvent.Movie.ImdbRating = imdbMovie.Rating;
-                                            movieEvent.Movie.ImdbVotes = imdbMovie.Votes;
-                                            if (!movieEvent.Year.HasValue)
-                                                movieEvent.Year = imdbMovie.Year;
-                                        }
+                                        movieEvent.Movie.ImdbRating = imdbMovie.Rating;
+                                        movieEvent.Movie.ImdbVotes = imdbMovie.Votes;
+                                        if (!movieEvent.Year.HasValue)
+                                            movieEvent.Year = imdbMovie.Year;
                                     }
                                 }
                             }
-
-                            db.SaveChanges();
-                        } 
-                    }
-                }
-
-                if (userId != null)
-                {
-                    var user = db.Users.Where(u => u.UserId == userId).SingleOrDefault();
-                    if (user != null)
-                    {
-                        RefreshRequestTime = user.RefreshRequestTime;
-                        LastRefreshRatingsTime = user.LastRefreshRatingsTime;
-                        LastRefreshSuccess = user.LastRefreshSuccess;
-                        user.Usages++;
-                        user.LastUsageTime = DateTime.UtcNow;
-                        ImdbUserId = user.ImdbUserId;
-                        db.SaveChanges();
-                    }
-                }
-
-                if (m.HasValue)
-                {
-                    if (m.Value == -2)
-                    {
-                        throw new Exception("Sentry test exception");
-                    }
-                    else
-                    {
-                        MovieEvent = db.MovieEvents.Find(m.Value);
-                        if (MovieEvent != null)
-                        {
-                            int days = (int)(MovieEvent.StartTime.Date - DateTime.Now.Date).TotalDays;
-                            if (FilterMaxDays < days)
-                                FilterMaxDays = days;
                         }
-                    }
+
+                        fxMoviesDbContext.SaveChanges();
+                    } 
                 }
-
-                Count = db.MovieEvents.Count();
-                CountTypeFilm = db.MovieEvents.Where(me => me.Type == 1).Count();
-                CountTypeShort = db.MovieEvents.Where(me => me.Type == 2).Count();
-                CountTypeSerie = db.MovieEvents.Where(me => me.Type == 3).Count();
-                CountMinRating5 = db.MovieEvents.Where(me => me.Movie.ImdbRating >= 50).Count();
-                CountMinRating6 = db.MovieEvents.Where(me => me.Movie.ImdbRating >= 60).Count();
-                CountMinRating7 = db.MovieEvents.Where(me => me.Movie.ImdbRating >= 70).Count();
-                CountMinRating8 = db.MovieEvents.Where(me => me.Movie.ImdbRating >= 80).Count();
-                CountMinRating9 = db.MovieEvents.Where(me => me.Movie.ImdbRating >= 90).Count();
-                CountNotOnImdb = db.MovieEvents.Where(me => string.IsNullOrEmpty(me.Movie.ImdbId)).Count();
-                CountNotRatedOnImdb = db.MovieEvents.Where(me => me.Movie.ImdbRating == null).Count();
-                CountCertNone =  db.MovieEvents.Where(me => string.IsNullOrEmpty(me.Movie.Certification)).Count();
-                CountCertG =  db.MovieEvents.Where(me => me.Movie.Certification == "US:G").Count();
-                CountCertPG =  db.MovieEvents.Where(me => me.Movie.Certification == "US:PG").Count();
-                CountCertPG13 =  db.MovieEvents.Where(me => me.Movie.Certification == "US:PG-13").Count();
-                CountCertR =  db.MovieEvents.Where(me => me.Movie.Certification == "US:R").Count();
-                CountCertNC17 =  db.MovieEvents.Where(me => me.Movie.Certification == "US:NC-17").Count();
-                CountCertOther =  Count - CountCertNone - CountCertG - CountCertPG - CountCertPG13 - CountCertR - CountCertNC17;
-                CountRated = db.MovieEvents.Count(me => me.Movie.UserRatings.Any(ur => ur.User.UserId == userId));
-                CountNotYetRated = Count - CountRated;
-                Count3days =  db.MovieEvents.Where(me => me.StartTime.Date <= now.Date.AddDays(3)).Count();
-                Count5days =  db.MovieEvents.Where(me => me.StartTime.Date <= now.Date.AddDays(5)).Count();
-                Count8days =  db.MovieEvents.Where(me => me.StartTime.Date <= now.Date.AddDays(8)).Count();
-
-                Records = db.MovieEvents
-                    .Where(me => 
-                        !me.Vod
-                        && 
-                        (FilterMaxDays == 0 || me.StartTime.Date <= now.Date.AddDays(FilterMaxDays))
-                        &&
-                        (me.EndTime >= now && me.StartTime >= now.AddMinutes(-30))
-                        &&
-                        (
-                            ((FilterTypeMask & 1) == 1 && me.Type == 1)
-                            || ((FilterTypeMask & 2) == 2 && me.Type == 2)
-                            || ((FilterTypeMask & 4) == 4 && me.Type == 3)
-                        )
-                        &&
-                        (!FilterMinRating.HasValue 
-                            || (FilterMinRating.Value == NO_IMDB_ID && string.IsNullOrEmpty(me.Movie.ImdbId))
-                            || (FilterMinRating.Value == NO_IMDB_RATING && me.Movie.ImdbRating == null)
-                            || (FilterMinRating.Value >= 0.0m && (me.Movie.ImdbRating >= FilterMinRating.Value * 10)))
-                        // && 
-                        // (FilterCert == Cert.all || (ParseCertification(me.Movie.Certification) & FilterCert) != 0)
-                    )
-                    .Include(me => me.Channel)
-                    .Include(me => me.Movie)
-                    .Select(me => new Record()
-                        {
-                            MovieEvent = me,
-                            UserRating = me.Movie.UserRatings.FirstOrDefault(ur => ur.User.UserId == userId),
-                            UserWatchListItem = me.Movie.UserWatchListItems.FirstOrDefault(ur => ur.User.UserId == userId)
-                        }
-                    )
-                    .ToList();
             }
+
+            if (userId != null)
+            {
+                var user = fxMoviesDbContext.Users.Where(u => u.UserId == userId).SingleOrDefault();
+                if (user != null)
+                {
+                    RefreshRequestTime = user.RefreshRequestTime;
+                    LastRefreshRatingsTime = user.LastRefreshRatingsTime;
+                    LastRefreshSuccess = user.LastRefreshSuccess;
+                    user.Usages++;
+                    user.LastUsageTime = DateTime.UtcNow;
+                    ImdbUserId = user.ImdbUserId;
+                    fxMoviesDbContext.SaveChanges();
+                }
+            }
+
+            if (m.HasValue)
+            {
+                if (m.Value == -2)
+                {
+                    throw new Exception("Sentry test exception");
+                }
+                else
+                {
+                    MovieEvent = dbMovieEvents.SingleOrDefault(me => me.Id == m.Value);
+                    if (MovieEvent != null)
+                    {
+                        int days = (int)(MovieEvent.StartTime.Date - DateTime.Now.Date).TotalDays;
+                        if (FilterMaxDays < days)
+                            FilterMaxDays = days;
+                    }
+                }
+            }
+
+            Count = dbMovieEvents.Count();
+            CountTypeFilm = dbMovieEvents.Where(me => me.Type == 1).Count();
+            CountTypeShort = dbMovieEvents.Where(me => me.Type == 2).Count();
+            CountTypeSerie = dbMovieEvents.Where(me => me.Type == 3).Count();
+            CountMinRating5 = dbMovieEvents.Where(me => me.Movie.ImdbRating >= 50).Count();
+            CountMinRating6 = dbMovieEvents.Where(me => me.Movie.ImdbRating >= 60).Count();
+            CountMinRating7 = dbMovieEvents.Where(me => me.Movie.ImdbRating >= 70).Count();
+            CountMinRating8 = dbMovieEvents.Where(me => me.Movie.ImdbRating >= 80).Count();
+            CountMinRating9 = dbMovieEvents.Where(me => me.Movie.ImdbRating >= 90).Count();
+            CountNotOnImdb = dbMovieEvents.Where(me => string.IsNullOrEmpty(me.Movie.ImdbId)).Count();
+            CountNotRatedOnImdb = dbMovieEvents.Where(me => me.Movie.ImdbRating == null).Count();
+            CountCertNone =  dbMovieEvents.Where(me => string.IsNullOrEmpty(me.Movie.Certification)).Count();
+            CountCertG =  dbMovieEvents.Where(me => me.Movie.Certification == "US:G").Count();
+            CountCertPG =  dbMovieEvents.Where(me => me.Movie.Certification == "US:PG").Count();
+            CountCertPG13 =  dbMovieEvents.Where(me => me.Movie.Certification == "US:PG-13").Count();
+            CountCertR =  dbMovieEvents.Where(me => me.Movie.Certification == "US:R").Count();
+            CountCertNC17 =  dbMovieEvents.Where(me => me.Movie.Certification == "US:NC-17").Count();
+            CountCertOther =  Count - CountCertNone - CountCertG - CountCertPG - CountCertPG13 - CountCertR - CountCertNC17;
+            CountRated = dbMovieEvents.Count(me => me.Movie.UserRatings.Any(ur => ur.User.UserId == userId));
+            CountNotYetRated = Count - CountRated;
+            Count3days =  dbMovieEvents.Where(me => me.StartTime.Date <= now.Date.AddDays(3)).Count();
+            Count5days =  dbMovieEvents.Where(me => me.StartTime.Date <= now.Date.AddDays(5)).Count();
+            Count8days =  dbMovieEvents.Where(me => me.StartTime.Date <= now.Date.AddDays(8)).Count();
+
+            Records = dbMovieEvents
+                .Where(me => 
+                    !me.Vod
+                    && 
+                    (FilterMaxDays == 0 || me.StartTime.Date <= now.Date.AddDays(FilterMaxDays))
+                    &&
+                    (me.EndTime >= now && me.StartTime >= now.AddMinutes(-30))
+                    &&
+                    (
+                        ((FilterTypeMask & 1) == 1 && me.Type == 1)
+                        || ((FilterTypeMask & 2) == 2 && me.Type == 2)
+                        || ((FilterTypeMask & 4) == 4 && me.Type == 3)
+                    )
+                    &&
+                    (!FilterMinRating.HasValue 
+                        || (FilterMinRating.Value == NO_IMDB_ID && string.IsNullOrEmpty(me.Movie.ImdbId))
+                        || (FilterMinRating.Value == NO_IMDB_RATING && me.Movie.ImdbRating == null)
+                        || (FilterMinRating.Value >= 0.0m && (me.Movie.ImdbRating >= FilterMinRating.Value * 10)))
+                    // && 
+                    // (FilterCert == Cert.all || (ParseCertification(me.Movie.Certification) & FilterCert) != 0)
+                )
+                .Include(me => me.Channel)
+                .Include(me => me.Movie)
+                .Select(me => new Record()
+                    {
+                        MovieEvent = me,
+                        UserRating = me.Movie.UserRatings.FirstOrDefault(ur => ur.User.UserId == userId),
+                        UserWatchListItem = me.Movie.UserWatchListItems.FirstOrDefault(ur => ur.User.UserId == userId)
+                    }
+                )
+                .ToList();
         }
 
         private static Cert ParseCertification(string certification)
