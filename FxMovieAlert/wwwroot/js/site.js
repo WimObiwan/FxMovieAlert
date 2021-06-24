01317c118a34130ec662dbb40e16deec4c73a98b
WimObiwan <wim@obiwan.be>
2021-06-24 23:45:59 +0200

    Site: Added service-worjer, install button for PWA.

--- FxMovieAlert/wwwroot/js/site.js
+++ FxMovieAlert/wwwroot/js/site.js
@@ -7,10 +7,9 @@ jQuery(document).ready(function($) {
     //primary navigation slide-in effect
     if ($(window).width() >= MQL) {
         $(window).on('scroll', {
-            },
+            }, 
             function() {
                 var currentTop = $(window).scrollTop();
-
                 if (currentTop >= 150) {
                     $('.sideBar').fadeIn('slow');
                 } else {
@@ -19,3 +18,43 @@ jQuery(document).ready(function($) {
             });
     }
 });
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+            navigator.serviceWorker.register('/service-worker.js', { scope: '/' })
+            .then(function(registration) {
+            // Registration was successful
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, function(err) {
+            // registration failed :(
+            console.log('ServiceWorker registration failed: ', err);
+        });
+
+        butInstall.addEventListener('click', async () => {
+            console.log('👍', 'butInstall-clicked');
+            const promptEvent = window.deferredPrompt;
+            if (!promptEvent) {
+                // The deferred prompt isn't available.
+                return;
+            }
+            // Show the install prompt.
+            promptEvent.prompt();
+            // Log the result
+            const result = await promptEvent.userChoice;
+            console.log('👍', 'userChoice', result);
+            // Reset the deferred prompt variable, since
+            // prompt() can only be called once.
+            window.deferredPrompt = null;
+            // Hide the install button.
+            installContainer.classList.toggle('hidden', true);
+        });
+    });
+
+    window.addEventListener('beforeinstallprompt', (event) => {
+        console.log('👍', 'beforeinstallprompt', event);
+        // Stash the event so it can be triggered later.
+        window.deferredPrompt = event;
+        // Remove the 'hidden' class from the install button container
+        installContainer.classList.toggle('hidden', false);
+    });
+}
