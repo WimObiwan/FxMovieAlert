0078dc475c55435f5b032e5286187ca91d472509
WimObiwan <wim@obiwan.be>
2021-06-29 23:53:31 +0200

    Inject version info

---
+++ ImdbDB/AssemblyInfo.cs
@@ -0,0 +1,24 @@
+using System.Reflection;
+
+[assembly: AssemblyVersion (ThisAssembly.Git.SemVer.Major + "." + ThisAssembly.Git.SemVer.Minor + "." + ThisAssembly.Git.Commits)]
+
+[assembly: AssemblyFileVersion (ThisAssembly.Git.SemVer.Major + "." + ThisAssembly.Git.SemVer.Minor + "." + ThisAssembly.Git.Commits)]
+
+[assembly: AssemblyInformationalVersion (
+    ThisAssembly.Git.SemVer.Major + "." +
+    ThisAssembly.Git.SemVer.Minor + "." +
+    ThisAssembly.Git.Commits + "-" +
+    ThisAssembly.Git.Branch + "+" +
+	ThisAssembly.Git.Commit)]
+
+[assembly: AssemblyCompany("Fox Innovations")]
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+ 
+[assembly: AssemblyProduct("FilmOpTv")]
+
+[assembly: AssemblyTitle("FilmOpTv Web")]
