032b5c1022605610192d3ea2a2b05093858d6449
WimObiwan <wim@obiwan.be>
2021-08-01 22:50:16 +0200

    Site: Fixed bug with onlyHighlights filtering.

--- FxMovieAlert/Pages/BroadcastModelBase.cs
+++ FxMovieAlert/Pages/BroadcastModelBase.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
@@ -301,6 +301,7 @@ namespace FxMovieAlert.Pages
             if (FilterOnlyHighlights)
             {
                 tmp = tmp
+                    .Where(r => r.UserRating == null || r.Highlighted)
                     .OrderByDescending(r => r.Highlighted)
                     .ThenByDescending(r => r.MovieEvent.Movie.ImdbRating)
                     .Take(30);
