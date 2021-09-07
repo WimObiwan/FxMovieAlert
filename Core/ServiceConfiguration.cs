ac0ef4d9b03bce2ca32bf89ba1423e805136a048
WimObiwan <wim@obiwan.be>
2021-09-07 23:19:18 +0200

    Grabber/Core: Moved ServiceConfiguration to extension method

---
+++ Core/ServiceConfiguration.cs
@@ -0,0 +1,88 @@
+using System;
+using System.Reflection;
+using FxMovies.Core.Commands;
+using FxMovies.Core.Queries;
+using FxMovies.Core.Repositories;
+using FxMovies.Core.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FxMovies.Core
+{
+    public static class ServiceConfiguration
+    {
+        public static IServiceCollection AddFxMoviesCore(this IServiceCollection services, IConfiguration configuration, Assembly assembly)
+        {
+            services.AddHttpClient("goplay", c =>
+            {
+                c.BaseAddress = new Uri("https://www.goplay.be");
+            });
+            services.AddHttpClient("vtmgo_login", c =>
+            {
+                c.BaseAddress = new Uri("https://login2.vtm.be");
+                c.DefaultRequestHeaders.Add("User-Agent", "VTMGO/10.3 (be.vmma.vtm.zenderapp; build:13259; Android 25) okhttp/4.9.0");
+                c.DefaultRequestHeaders.Add("x-app-version", "10");
+                c.DefaultRequestHeaders.Add("x-persgroep-mobile-app", "true");
+                c.DefaultRequestHeaders.Add("x-persgroep-os", "android");
+                c.DefaultRequestHeaders.Add("x-persgroep-os-version", "25");
+            });
+            services.AddHttpClient("vtmgo_dpg", c =>
+            {
+                c.BaseAddress = new Uri("https://lfvp-api.dpgmedia.net");
+                c.DefaultRequestHeaders.Add("User-Agent", "VTMGO/10.3 (be.vmma.vtm.zenderapp; build:13259; Android 25) okhttp/4.9.0");
+                c.DefaultRequestHeaders.Add("x-app-version", "10");
+                c.DefaultRequestHeaders.Add("x-persgroep-mobile-app", "true");
+                c.DefaultRequestHeaders.Add("x-persgroep-os", "android");
+                c.DefaultRequestHeaders.Add("x-persgroep-os-version", "25");
+            });
+            services.AddHttpClient("vrtnu", c =>
+            {
+                c.BaseAddress = new Uri("https://vrtnu-api.vrt.be");
+            });
+            services.AddHttpClient("tmdb", c => 
+            {
+                c.BaseAddress = new Uri("https://api.themoviedb.org");
+            });
+            services.AddHttpClient("images");
+            services.AddHttpClient("imdb", c => 
+            {
+                c.BaseAddress = new Uri("https://www.imdb.com");
+            });
+
+            services.AddSingleton<IVersionInfo, VersionInfo>((_) => new VersionInfo(assembly));
+
+            services.AddScoped<IUpdateEpgCommand, UpdateEpgCommand>();
+            services.AddScoped<IGenerateImdbDatabaseCommand, GenerateImdbDatabaseCommand>();
+            services.AddScoped<IUpdateImdbUserDataCommand, UpdateImdbUserDataCommand>();
+            services.AddScoped<IUpdateAllImdbUsersDataCommand, UpdateAllImdbUsersDataCommand>();
+            services.AddScoped<IAutoUpdateImdbUserDataCommand, AutoUpdateImdbUserDataCommand>();
+
+            services.AddScoped<IImdbMatchingQuery, ImdbMatchingQuery>();
+            services.AddScoped<IManualMatchesQuery, ManualMatchesQuery>();
+            services.AddScoped<IListManualMatchesQuery, ListManualMatchesQuery>();
+            services.AddScoped<IStatsQuery, StatsQuery>();
+
+            services.Configure<TheMovieDbServiceOptions>(configuration.GetSection(TheMovieDbServiceOptions.Position));
+            services.Configure<VtmGoServiceOptions>(configuration.GetSection(VtmGoServiceOptions.Position));
+            services.Configure<UpdateEpgCommandOptions>(configuration.GetSection(UpdateEpgCommandOptions.Position));
+            services.Configure<GenerateImdbDatabaseCommandOptions>(configuration.GetSection(GenerateImdbDatabaseCommandOptions.Position));
+            services.Configure<AutoUpdateImdbUserDataCommandOptions>(configuration.GetSection(AutoUpdateImdbUserDataCommandOptions.Position));
+            services.Configure<ImdbMatchingQueryOptions>(configuration.GetSection(ImdbMatchingQueryOptions.Position));
+
+            services.AddScoped<IMovieCreationHelper, MovieCreationHelper>();
+            services.AddScoped<IImdbRatingsFromWebService, ImdbRatingsFromWebService>();
+            services.AddScoped<IImdbWatchlistFromWebService, ImdbWatchlistFromWebService>();
+            services.AddScoped<ITheMovieDbService, TheMovieDbService>();
+            services.AddScoped<IMovieEventService, GoPlayService>();
+            services.AddScoped<IMovieEventService, VtmGoService>();
+            services.AddScoped<IMovieEventService, VrtNuService>();
+            services.AddScoped<IHumoService, HumoService>();
+
+            services.AddScoped<IUserRatingsRepository, UserRatingsRepository>();
+            services.AddScoped<IUserWatchlistRepository, UserWatchlistRepository>();
+            services.AddScoped<IUsersRepository, UsersRepository>();
+
+            return services;
+        }
+    }
+}
--- Grabber/Startup.cs
+++ Grabber/Startup.cs
@@ -1,8 +1,6 @@
-using System;
 using FxMovies.Core;
 using FxMovies.Core.Commands;
 using FxMovies.Core.Queries;
-using FxMovies.Core.Repositories;
 using FxMovies.Core.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -12,89 +10,29 @@ namespace FxMovies.Grabber
 {
     public class Startup
     {
-        public IConfiguration Configuration { get; }
+        private readonly IConfiguration configuration;
 
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            this.configuration = configuration;
         }
 
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<FxMovies.FxMoviesDB.FxMoviesDbContext>(options =>
-                options.UseSqlite(Configuration.GetConnectionString("FxMoviesDB")));
+                options.UseSqlite(configuration.GetConnectionString("FxMoviesDB")));
 
             services.AddDbContext<FxMovies.ImdbDB.ImdbDbContext>(options =>
-                options.UseSqlite(Configuration.GetConnectionString("ImdbDb")));
+                options.UseSqlite(configuration.GetConnectionString("ImdbDb")));
 
-            services.AddHttpClient("goplay", c =>
-            {
-                c.BaseAddress = new Uri("https://www.goplay.be");
-            });
-            services.AddHttpClient("vtmgo_login", c =>
-            {
-                c.BaseAddress = new Uri("https://login2.vtm.be");
-                c.DefaultRequestHeaders.Add("User-Agent", "VTMGO/10.3 (be.vmma.vtm.zenderapp; build:13259; Android 25) okhttp/4.9.0");
-                c.DefaultRequestHeaders.Add("x-app-version", "10");
-                c.DefaultRequestHeaders.Add("x-persgroep-mobile-app", "true");
-                c.DefaultRequestHeaders.Add("x-persgroep-os", "android");
-                c.DefaultRequestHeaders.Add("x-persgroep-os-version", "25");
-            });
-            services.AddHttpClient("vtmgo_dpg", c =>
-            {
-                c.BaseAddress = new Uri("https://lfvp-api.dpgmedia.net");
-                c.DefaultRequestHeaders.Add("User-Agent", "VTMGO/10.3 (be.vmma.vtm.zenderapp; build:13259; Android 25) okhttp/4.9.0");
-                c.DefaultRequestHeaders.Add("x-app-version", "10");
-                c.DefaultRequestHeaders.Add("x-persgroep-mobile-app", "true");
-                c.DefaultRequestHeaders.Add("x-persgroep-os", "android");
-                c.DefaultRequestHeaders.Add("x-persgroep-os-version", "25");
-            });
-            services.AddHttpClient("vrtnu", c =>
-            {
-                c.BaseAddress = new Uri("https://vrtnu-api.vrt.be");
-            });
-            services.AddHttpClient("tmdb", c => 
-            {
-                c.BaseAddress = new Uri("https://api.themoviedb.org");
-            });
-            services.AddHttpClient("images");
-            services.AddHttpClient("imdb", c => 
-            {
-                c.BaseAddress = new Uri("https://www.imdb.com");
-            });
+            services.Configure<TheMovieDbServiceOptions>(configuration.GetSection(TheMovieDbServiceOptions.Position));
+            services.Configure<VtmGoServiceOptions>(configuration.GetSection(VtmGoServiceOptions.Position));
+            services.Configure<UpdateEpgCommandOptions>(configuration.GetSection(UpdateEpgCommandOptions.Position));
+            services.Configure<GenerateImdbDatabaseCommandOptions>(configuration.GetSection(GenerateImdbDatabaseCommandOptions.Position));
+            services.Configure<AutoUpdateImdbUserDataCommandOptions>(configuration.GetSection(AutoUpdateImdbUserDataCommandOptions.Position));
+            services.Configure<ImdbMatchingQueryOptions>(configuration.GetSection(ImdbMatchingQueryOptions.Position));
 
-            services.AddSingleton<IVersionInfo, VersionInfo>((_) => new VersionInfo(typeof(Startup).Assembly));
-
-            services.AddScoped<IUpdateEpgCommand, UpdateEpgCommand>();
-            services.AddScoped<IGenerateImdbDatabaseCommand, GenerateImdbDatabaseCommand>();
-            services.AddScoped<IUpdateImdbUserDataCommand, UpdateImdbUserDataCommand>();
-            services.AddScoped<IUpdateAllImdbUsersDataCommand, UpdateAllImdbUsersDataCommand>();
-            services.AddScoped<IAutoUpdateImdbUserDataCommand, AutoUpdateImdbUserDataCommand>();
-
-            services.AddScoped<IImdbMatchingQuery, ImdbMatchingQuery>();
-            services.AddScoped<IManualMatchesQuery, ManualMatchesQuery>();
-            services.AddScoped<IListManualMatchesQuery, ListManualMatchesQuery>();
-            services.AddScoped<IStatsQuery, StatsQuery>();
-
-            services.Configure<TheMovieDbServiceOptions>(Configuration.GetSection(TheMovieDbServiceOptions.Position));
-            services.Configure<VtmGoServiceOptions>(Configuration.GetSection(VtmGoServiceOptions.Position));
-            services.Configure<UpdateEpgCommandOptions>(Configuration.GetSection(UpdateEpgCommandOptions.Position));
-            services.Configure<GenerateImdbDatabaseCommandOptions>(Configuration.GetSection(GenerateImdbDatabaseCommandOptions.Position));
-            services.Configure<AutoUpdateImdbUserDataCommandOptions>(Configuration.GetSection(AutoUpdateImdbUserDataCommandOptions.Position));
-            services.Configure<ImdbMatchingQueryOptions>(Configuration.GetSection(ImdbMatchingQueryOptions.Position));
-
-            services.AddScoped<IMovieCreationHelper, MovieCreationHelper>();
-            services.AddScoped<IImdbRatingsFromWebService, ImdbRatingsFromWebService>();
-            services.AddScoped<IImdbWatchlistFromWebService, ImdbWatchlistFromWebService>();
-            services.AddScoped<ITheMovieDbService, TheMovieDbService>();
-            services.AddScoped<IMovieEventService, GoPlayService>();
-            services.AddScoped<IMovieEventService, VtmGoService>();
-            services.AddScoped<IMovieEventService, VrtNuService>();
-            services.AddScoped<IHumoService, HumoService>();
-
-            services.AddScoped<IUserRatingsRepository, UserRatingsRepository>();
-            services.AddScoped<IUserWatchlistRepository, UserWatchlistRepository>();
-            services.AddScoped<IUsersRepository, UsersRepository>();
+            services.AddFxMoviesCore(configuration, typeof(Program).Assembly);
         }
     }
 }
