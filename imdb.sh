e84e8f4291ea4b099eeab4541991df78a32e8623
WimObiwan <wim@obiwan.be>
2021-10-06 22:58:31 +0200

    Added script to update and upload imdb.db

---
+++ imdb.sh
@@ -0,0 +1,11 @@
+
+export TARGET="www"
+
+wget https://datasets.imdbws.com/title.akas.tsv.gz -O ~/tmp/filmoptv/title.akas.tsv.gz
+wget https://datasets.imdbws.com/title.basics.tsv.gz -O ~/tmp/filmoptv/title.basics.tsv.gz
+wget https://datasets.imdbws.com/title.ratings.tsv.gz -O ~/tmp/filmoptv/title.ratings.tsv.gz
+
+cd Grabber
+dotnet ./bin/Release/net5.0/Grabber.dll GenerateImdbDatabase
+
+rsync -av --info=progress2 ~/tmp/filmoptv/imdb.db ./Grabber/bin/Release/$RELEASE/publish/* root@server3.foxinnovations.be:/var/www/$TARGET.filmoptv.be/
