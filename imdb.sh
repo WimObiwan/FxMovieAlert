779ae0eacc675779f527ff21c080c32a941a794c
WimObiwan <wim@obiwan.be>
2021-11-15 22:03:18 +0100

    Updated to .net 6

--- imdb.sh
+++ imdb.sh
@@ -6,6 +6,6 @@ wget https://datasets.imdbws.com/title.basics.tsv.gz -O ~/tmp/filmoptv/title.bas
 wget https://datasets.imdbws.com/title.ratings.tsv.gz -O ~/tmp/filmoptv/title.ratings.tsv.gz
 
 cd Grabber
-dotnet ./bin/Release/net5.0/Grabber.dll GenerateImdbDatabase
+dotnet ./bin/Release/net6.0/Grabber.dll GenerateImdbDatabase
 
 rsync -av --info=progress2 ~/tmp/filmoptv/imdb.db root@server3.foxinnovations.be:/var/www/$TARGET.filmoptv.be/
