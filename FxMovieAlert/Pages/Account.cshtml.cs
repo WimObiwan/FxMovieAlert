bcd2c71fc59e88043db4e8b13a102bad53ec3d8e
WimObiwan <wim@obiwan.be>
2021-12-05 23:11:02 +0100

    Site: Added proxying off avatar to get around browsers blocking facebook avatar loading

--- FxMovieAlert/Pages/Account.cshtml.cs
+++ FxMovieAlert/Pages/Account.cshtml.cs
@@ -1,6 +1,10 @@
-﻿using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace FxMovieAlert.Pages;
@@ -30,5 +34,23 @@ public class AccountModel : PageModel
             RedirectUri = returnUrl
         });
         await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+    }
+
+    public async Task<IActionResult> OnGetAvatar(CancellationToken cancellationToken)
+    {
+        var picture = User.FindFirst("picture")?.Value;
+
+        var httpClient = new HttpClient();
+        using var response = await httpClient.GetAsync(picture, cancellationToken);
+
+        Response.StatusCode = (int)response.StatusCode;
+        foreach (var header in response.Headers) Response.Headers[header.Key] = header.Value.ToArray();
+
+        foreach (var header in response.Content.Headers) Response.Headers[header.Key] = header.Value.ToArray();
+
+        // SendAsync removes chunking from the response. This removes the header so it doesn't expect a chunked response.
+        Response.Headers.Remove("transfer-encoding");
+        await response.Content.CopyToAsync(Response.Body);
+        return new EmptyResult();
     }
 }
