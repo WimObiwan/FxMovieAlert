5fe5bbe340cc776b434d77de712ade079fd3c058
WimObiwan <wim@obiwan.be>
2021-12-08 23:00:14 +0100

    Site: Moved Authentication configuration to extension methods

--- Site/Program.cs
+++ Site/Program.cs
@@ -1,15 +1,12 @@
 ﻿using System;
 using System.Globalization;
 using System.IO;
-using System.Security.Claims;
 using System.Threading.Tasks;
 using FxMovies.Core;
 using FxMovies.ImdbDB;
 using FxMovies.MoviesDB;
 using FxMovies.Site.HealthChecks;
 using FxMovies.Site.Options;
-using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Hosting;
@@ -93,113 +90,7 @@ public static class Program
             options.MinimumSameSitePolicy = SameSiteMode.None;
         });
 
-        services.AddAuthentication(options =>
-            {
-                options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-                options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-                options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-            })
-            .AddCookie(options =>
-            {
-                // add an instance of the patched manager to the options:
-                options.CookieManager = new ChunkingCookieManager();
-                options.ExpireTimeSpan = TimeSpan.FromDays(31);
-                options.SlidingExpiration = true;
-
-                options.Cookie.HttpOnly = true;
-                options.Cookie.SameSite = SameSiteMode.None;
-                options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
-            })
-            .AddOpenIdConnect("Auth0", options =>
-            {
-                // Set the authority to your Auth0 domain
-                options.Authority = $"https://{configuration["Auth0:Domain"]}";
-
-                // Configure the Auth0 Client ID and Client Secret
-                options.ClientId = configuration["Auth0:ClientId"];
-                options.ClientSecret = configuration["Auth0:ClientSecret"];
-
-                // Set response type to code
-                options.ResponseType = "code";
-
-                options.SaveTokens = true;
-
-                // Configure the scope
-                options.Scope.Clear();
-                options.Scope.Add("openid");
-                options.Scope.Add("profile");
-
-                // Set the callback path, so Auth0 will call back to http://localhost:5000/signin-auth0 
-                // Also ensure that you have added the URL as an Allowed Callback URL in your Auth0 dashboard 
-                options.CallbackPath = new PathString("/signin-auth0");
-
-                // Configure the Claims Issuer to be Auth0
-                options.ClaimsIssuer = "Auth0";
-
-                options.Events = new OpenIdConnectEvents
-                {
-                    OnTicketReceived = context =>
-                    {
-                        // Get the ClaimsIdentity
-                        if (context.Principal?.Identity is ClaimsIdentity identity)
-                        {
-                            // Add the Name ClaimType. This is required if we want User.Identity.Name to actually return something!
-                            if (!context.Principal.HasClaim(c => c.Type == ClaimTypes.Name))
-                            {
-                                var name = identity.FindFirst("name")?.Value;
-                                if (name != null)
-                                    identity.AddClaim(new Claim(ClaimTypes.Name, name));
-                            }
-
-                            // Check if token names are stored in Properties
-                            var items = context.Properties?.Items;
-                            if (items != null
-                                && items.TryGetValue(".TokenNames", out var tokenNamesString)
-                                && tokenNamesString != null)
-                            {
-                                // Token names a semicolon separated
-                                var tokenNames = tokenNamesString.Split(';');
-
-                                // Add each token value as Claim
-                                foreach (var tokenName in tokenNames)
-                                    // Tokens are stored in a Dictionary with the Key ".Token.<token name>"
-                                    if (items.TryGetValue($".Token.{tokenName}", out var tokenValue) &&
-                                        tokenValue != null)
-                                        identity.AddClaim(new Claim(tokenName, tokenValue));
-                            }
-                        }
-
-                        return Task.CompletedTask;
-                    },
-
-                    // handle the logout redirection 
-                    OnRedirectToIdentityProviderForSignOut = context =>
-                    {
-                        var logoutUri =
-                            $"https://{configuration["Auth0:Domain"]}/v2/logout?client_id={configuration["Auth0:ClientId"]}";
-
-                        var postLogoutUri = context.Properties.RedirectUri;
-                        if (!string.IsNullOrEmpty(postLogoutUri))
-                        {
-                            if (postLogoutUri.StartsWith("/"))
-                            {
-                                // transform to absolute
-                                var request = context.Request;
-                                postLogoutUri = request.Scheme + "://" + request.Host + request.PathBase +
-                                                postLogoutUri;
-                            }
-
-                            logoutUri += $"&returnTo={Uri.EscapeDataString(postLogoutUri)}";
-                        }
-
-                        context.Response.Redirect(logoutUri);
-                        context.HandleResponse();
-
-                        return Task.CompletedTask;
-                    }
-                };
-            });
-
+        services.AddFxMoviesAuthentication(configuration);
 
         services.Configure<ForwardedHeadersOptions>(options =>
         {
